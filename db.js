75cca7992deb90a13eebf18f149c7ab073e7d25e
Cheryl Stone <cheryl.stone34@gmail.com>
2021-11-23 22:05:33 -0500

    deploy

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  // ssl: process.env.ENVIRONMENT === 'production'
+  ssl: process.env.ENVIRONMENT === 'production',
   dialectOptions: {
     ssl: {
         require: true,
